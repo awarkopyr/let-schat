e5304cf13a5ffec6f70fd6219afc4c93394ac3f6
awarkopyr <awarkohm2002@gmail.com>
2024-09-03 19:26:05 +0600

    Deploy

--- backend/server.js
+++ backend/server.js
@@ -7,15 +7,23 @@ import messageRoutes from "./routes/message.routes.js"
 import connectToMongoDB from './db/connectToMongoDB.js'
 
 import { app, server } from './socket/socket.js'
+import path from 'path'
 
-dotenv.config()
 const PORT = process.env.PORT || 5000
+const __dirname = path.resolve()
+dotenv.config()
 
 app.use(express.json())
 app.use(cookieParser())
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
 app.use("/api/users", userRoutes)
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
     connectToMongoDB()
